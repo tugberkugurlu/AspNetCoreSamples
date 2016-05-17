42a0dc85a69b2bf672857a437ddd9ff8f0a957eb
Tugberk Ugurlu <tugberk@outlook.com>
2016-05-17 05:03:40 -0700

    added API client

---
+++ haproxy-zero-downtime-sample/api-client/Program.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ApiClient
+{   
+    public static class Program 
+    {
+        public static int Main(string[] args)
+        {
+            var config = ConfigBuilder.Build();
+            var settings = config.Get<ApiClientSettings>();
+            
+            if(settings?.ApiBaseUrl == null)
+            {
+                Console.Error.WriteLine("Error: Specify ApiBaseUrl through ApiClient_ApiBaseUrl environment variable.");
+                return -1;
+            }
+            
+            using(var client = new HttpClient { BaseAddress = new Uri(settings.ApiBaseUrl) })
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine($"Fething at {DateTime.UtcNow}");
+                        
+                        var response = client.GetAsync("cars").Result;
+                        var cars = JsonConvert.DeserializeObject<IEnumerable<Car>>(response.Content.ReadAsStringAsync().Result);
+                        foreach (var car in cars)
+                        {
+                            Console.WriteLine(car.Make + ", " + car.Model);
+                        }
+                        
+                        Console.WriteLine(Environment.NewLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine($"Error: {ex.Message}");
+                    }
+                    
+                    Task.Delay(100).Wait();
+                }
+            }
+            
+            return 0;
+        }
+    }
+    
+    public static class ConfigBuilder
+    {   
+        public static IConfiguration Build()
+        {
+            return new ConfigurationBuilder()
+                .AddEnvironmentVariables("ApiClient_")
+                .Build();
+        }
+    }
+    
+    public class ApiClientSettings
+    {
+        public string ApiBaseUrl { get; set; }
+    }
+    
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
