51578d90040158d8ef5e91b68ce7be50e2c84968
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-10-08 16:49:15 +0100

    ModelState serialization is inconsistent with validation attributes repro

---
+++ repros/ValidationJsonSerialize/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApplication
+{
+    public class PaginatedRequestCommand
+    {
+        [Range(0, int.MaxValue)]
+        public int Skip { get; set; }
+
+        [Range(1, 50)]
+        public int Take { get; set; }
+    }
+    
+    public class ValuesController : Controller 
+    {
+        [HttpGet("values")]
+        public IActionResult Get(int skip, int take)
+        {
+            var requestCommand = new PaginatedRequestCommand
+            {
+                Skip = skip, Take = take
+            };
+
+            return TryValidateModel(requestCommand) ?
+                Ok(new [] { "value1", "value2" }) :
+                BadRequest(ModelState) as IActionResult;
+        }
+    }
+
+    public class Startup 
+    {
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) 
+        {
+            app.UseMvc();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
