aaef8edb7cfadd7994642a341866a575127dc5a7
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2016-10-21 19:55:05 +0100

    added Docker DotNet sample

---
+++ prototypes/DockerNetClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock"))
+                .CreateClient();
+
+            var containers = client.Containers.ListContainersAsync(
+                new ContainersListParameters(){
+                    Limit = 10,
+                }).Result;
+
+            foreach (var container in containers)
+            {
+                System.Console.WriteLine(string.Join(",", container.Names));
+            }
+        }
+    }
+}
