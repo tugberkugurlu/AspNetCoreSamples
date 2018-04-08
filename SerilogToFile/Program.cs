12932de7998022f280cebe3da2428c7f25608192
Tugberk Ugurlu <tugberk@outlook.com>
2018-04-08 11:49:42 +0100

    SerilogToFile sample

---
+++ SerilogToFile/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace SerilogToFile
+{
+    public static class Program
+    {
+        private static IConfiguration _configuration = new ConfigurationBuilder()
+            .ConfigureAppConfig()
+            .Build();
+
+        public static int Main(string[] args)
+        {
+            var logsFilePath = GetLogsFilePath();
+            var serilogger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(new CompactJsonFormatter(), logsFilePath, rollingInterval: RollingInterval.Minute)
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            Console.WriteLine($"Logs are being written to {logsFilePath}");
+
+            // Log.Logger = serilogger;
+
+            try
+            {
+                serilogger.Information("Starting web host");
+                BuildWebHost(args, serilogger).Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                serilogger.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                // Log.CloseAndFlush();
+            }
+        }
+
+        private static string GetLogsFilePath()
+        {
+#if DEBUG
+            var logsFolder = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "Logs");
+#else
+            var appDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var logsFolder = Path.Combine(appDataFolder, "Local", "AspNetCoreSamples", "Logs", "SerilogToFileSample");
+#endif
+            return Path.Combine(logsFolder, "logs-.txt");
+        }
+
+        public static IWebHost BuildWebHost(string[] args, Serilog.ILogger serilogger) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, builder) => builder.ConfigureAppConfig())
+                .UseSerilog(serilogger, true)
+                .UseStartup<Startup>()
+                .Build();
+
+        private static IConfigurationBuilder ConfigureAppConfig(this IConfigurationBuilder builder) =>
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables("SerilogToFileSample");
+    }
+}
---
+++ SerilogToFile/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogToFile
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                try
+                {
+                    throw new InvalidOperationException();
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error while starting up under {EnvironmentName}", env.EnvironmentName);
+                }
+
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
