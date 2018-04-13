607156667970fb263d362ce0327e8a827797dfb6
Tugberk Ugurlu <tugberk@outlook.com>
2018-04-13 20:52:15 +0100

    this works
    not all cases. e.g.
    
     - you can still request /limits along with /interal/limits. That's sad.
     - you cannot share dependecnies from ConfigureServices. Each pipeline is totally isolated.
     - any dependency being passed from the hosting layer is now ignored inside Configure pipeline

--- MvcActionSelection/MvcApp/Startup.cs
+++ MvcActionSelection/MvcApp/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApiContrib.Core;
 
 namespace MvcApp
 {
@@ -37,6 +41,18 @@ namespace MvcApp
         public string GetHeaderMessage() => $"internal-{_messageProvider.GetMessage()}";
     }
 
+    public class PublicHeaderMessageProvider : IHeaderMessageProvider
+    {
+        private readonly IMessageProvider _messageProvider;
+
+        public PublicHeaderMessageProvider(IMessageProvider messageProvider)
+        {
+            _messageProvider = messageProvider ?? throw new ArgumentNullException(nameof(messageProvider));
+        }
+
+        public string GetHeaderMessage() => $"public-{_messageProvider.GetMessage()}";
+    }
+
     public class DefaultHeaderAppenderMiddleware
     {
         private readonly RequestDelegate _next;
@@ -56,12 +72,12 @@ namespace MvcApp
         }
     }
 
-    public class InternalHeaderAppenderMiddleware
+    public class ProductHeaderAppenderMiddleware
     {
         private readonly RequestDelegate _next;
         private readonly IHeaderMessageProvider _headerMessageProvider;
 
-        public InternalHeaderAppenderMiddleware(RequestDelegate next, IHeaderMessageProvider headerMessageProvider)
+        public ProductHeaderAppenderMiddleware(RequestDelegate next, IHeaderMessageProvider headerMessageProvider)
         {
             _next = next ?? throw new ArgumentNullException(nameof(next));
             _headerMessageProvider = headerMessageProvider ?? throw new ArgumentNullException(nameof(headerMessageProvider));
@@ -75,20 +91,45 @@ namespace MvcApp
         }
     }
 
+    // https://www.strathweb.com/2017/04/running-multiple-independent-asp-net-core-pipelines-side-by-side-in-the-same-application/
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddSingleton<IMessageProvider, DefaultMessageProvider>();
-            services.AddSingleton<IHeaderMessageProvider, InternalHeaderMessageProvider>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMiddleware<DefaultHeaderAppenderMiddleware>();
-            app.UseMiddleware<InternalHeaderAppenderMiddleware>();
-            app.UseMvc();
+            
+            app.UseBranchWithServices("/internal", 
+                internalServices => 
+                {
+                    internalServices.AddMvc();
+                    internalServices.AddSingleton<IMessageProvider, DefaultMessageProvider>();
+                    internalServices.AddSingleton<IHeaderMessageProvider, InternalHeaderMessageProvider>();
+                },
+
+                internalApp => 
+                {
+                    internalApp.UseMiddleware<ProductHeaderAppenderMiddleware>();
+                    internalApp.UseMvc();
+                });
+
+            app.UseBranchWithServices("/api", 
+                publicServices => 
+                {
+                    publicServices.AddMvc();
+                    publicServices.AddSingleton<IMessageProvider, DefaultMessageProvider>();
+                    publicServices.AddSingleton<IHeaderMessageProvider, PublicHeaderMessageProvider>();
+                },
+
+                publicApp => 
+                {
+                    publicApp.UseMiddleware<ProductHeaderAppenderMiddleware>();
+                    publicApp.UseMvc();
+                });
         }
     }
 }
