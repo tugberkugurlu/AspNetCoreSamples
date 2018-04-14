e3437c3e28b588304a3334135e56373d0b94bf71
Tugberk Ugurlu <tugberk@outlook.com>
2018-04-14 15:50:44 +0100

    first working pass on startup middleware order test

---
+++ AppBuilderTests/WebApp.Tests/ApplicationBuilderTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.AspNetCore.Hosting.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace WebApp.Tests
+{
+    public class TrackingApplicationBuilder : IApplicationBuilder
+    {
+        private readonly IApplicationBuilder _appBuilder;
+        private readonly IList<Type> _registeredMiddlewares;
+
+        public TrackingApplicationBuilder()
+        {
+            _appBuilder = new ApplicationBuilder(new DummyServiceProvider());
+            _registeredMiddlewares = new List<Type>();;
+            ApplicationServices = _appBuilder.ApplicationServices;
+        }
+
+        public IEnumerable<Type> RegisteredMiddlewares => _registeredMiddlewares;
+
+        public IServiceProvider ApplicationServices { get; set; }
+
+        public IFeatureCollection ServerFeatures => _appBuilder.ServerFeatures;
+
+        public IDictionary<string, object> Properties => _appBuilder.Properties;
+
+        public RequestDelegate Build()
+        {
+            return _appBuilder.Build();
+        }
+
+        public IApplicationBuilder New()
+        {
+            return _appBuilder.New();
+        }
+
+        public IApplicationBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
+        {
+            try 
+            {
+                var appliedMiddleware = middleware.Target
+                    .GetType()
+                    .GetField("middleware")
+                    .GetValue(middleware.Target);
+
+                if(appliedMiddleware is Type) 
+                {
+                    _registeredMiddlewares.Add((Type)appliedMiddleware);
+                }
+                else 
+                {
+                    var middlewareType = appliedMiddleware.GetType();
+                    _registeredMiddlewares.Add(middlewareType);
+                }
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return _appBuilder.Use(middleware);
+        }
+
+        private class DummyServiceProvider : IServiceProvider
+        {
+            private Dictionary<Type, object> _services = new Dictionary<Type, object>();
+
+            public void AddService(Type type, object value) => _services[type] = value;
+
+            public object GetService(Type serviceType)
+            {
+                if (serviceType == typeof(IServiceProvider))
+                {
+                    return this;
+                }
+
+                if (_services.TryGetValue(serviceType, out object value))
+                {
+                    return value;
+                }
+                return null;
+            }
+        }
+    }
+
+    public class ApplicationBuilderTests
+    {   
+        [Fact]
+        public void DefaultHeaderAppenderMiddleware_ShouldBeRegisteredBeforeProductHeaderAppenderMiddleware()
+        {
+            var trackingAppBuilder = new TrackingApplicationBuilder();
+            new Startup().Configure(trackingAppBuilder);
+
+            bool defaultHeaderAppenderMiddlewareFound = false;
+            bool productHeaderAppenderMiddlewareFound = false;
+            
+            foreach (var registeredMiddleware in trackingAppBuilder.RegisteredMiddlewares)
+            {
+                productHeaderAppenderMiddlewareFound = registeredMiddleware == typeof(ProductHeaderAppenderMiddleware);
+                defaultHeaderAppenderMiddlewareFound = registeredMiddleware == typeof(DefaultHeaderAppenderMiddleware);
+
+                if(productHeaderAppenderMiddlewareFound) 
+                {
+                    Assert.False(true, $"{nameof(DefaultHeaderAppenderMiddleware)} should have been registered before {nameof(ProductHeaderAppenderMiddleware)}");
+                    break;
+                }
+
+                if(defaultHeaderAppenderMiddlewareFound)
+                {
+                    Assert.True(true, $"{nameof(DefaultHeaderAppenderMiddleware)} has been registered before {nameof(ProductHeaderAppenderMiddleware)}");
+                    break;
+                }
+            }
+
+            if(!defaultHeaderAppenderMiddlewareFound) 
+            {
+                Assert.False(true, $"{nameof(DefaultHeaderAppenderMiddleware)} should have been registered");
+            }
+        }
+    }
+}
--- AppBuilderTests/WebApp/Startup.cs
+++ AppBuilderTests/WebApp/Startup.cs
@@ -9,21 +9,98 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WebApp
 {
-    public class Startup
+    public interface IMessageProvider
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        string GetMessage();
+    }
+
+    public class DefaultMessageProvider : IMessageProvider
+    {
+        public string GetMessage() => Guid.NewGuid().ToString();
+    }
+
+    public interface IHeaderMessageProvider
+    {
+        string GetHeaderMessage();
+    }
+
+    public class InternalHeaderMessageProvider : IHeaderMessageProvider
+    {
+        private readonly IMessageProvider _messageProvider;
+
+        public InternalHeaderMessageProvider(IMessageProvider messageProvider)
         {
+            _messageProvider = messageProvider ?? throw new ArgumentNullException(nameof(messageProvider));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public string GetHeaderMessage() => $"internal-{_messageProvider.GetMessage()}";
+    }
+
+    public class PublicHeaderMessageProvider : IHeaderMessageProvider
+    {
+        private readonly IMessageProvider _messageProvider;
+
+        public PublicHeaderMessageProvider(IMessageProvider messageProvider)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            _messageProvider = messageProvider ?? throw new ArgumentNullException(nameof(messageProvider));
+        }
+
+        public string GetHeaderMessage() => $"public-{_messageProvider.GetMessage()}";
+    }
+
+    public class DefaultHeaderAppenderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IMessageProvider _messageProvider;
+
+        public DefaultHeaderAppenderMiddleware(RequestDelegate next, IMessageProvider messageProvider)
+        {
+            _messageProvider = messageProvider ?? throw new ArgumentNullException(nameof(messageProvider));
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("X-DEFAULT-HEADER", _messageProvider.GetMessage());
+
+            return _next(context);
+        }
+    }
+
+    public class ProductHeaderAppenderMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHeaderMessageProvider _headerMessageProvider;
+
+        public ProductHeaderAppenderMiddleware(RequestDelegate next, IHeaderMessageProvider headerMessageProvider)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+            _headerMessageProvider = headerMessageProvider ?? throw new ArgumentNullException(nameof(headerMessageProvider));
+        }
+
+        public Task Invoke(HttpContext context)
+        {
+            context.Response.Headers.Add("X-PRODUCT-HEADER", _headerMessageProvider.GetHeaderMessage());
+
+            return _next(context);
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMessageProvider, DefaultMessageProvider>();
+            services.AddSingleton<IHeaderMessageProvider, PublicHeaderMessageProvider>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseCors(options => options.AllowAnyOrigin());
+            app.UseMiddleware<DefaultHeaderAppenderMiddleware>();
+            app.UseMiddleware<ProductHeaderAppenderMiddleware>();
 
             app.Run(async (context) =>
             {
